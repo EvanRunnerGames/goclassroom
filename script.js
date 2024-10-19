c2420ec6cbec0900fb461a679ec5a46d32cd7222
EvanRunner <evanrunnercontact@gmail.com>
2024-10-18 21:44:38 -0700

    update

---
+++ script.js
@@ -0,0 +1,31 @@
+// Show the "Create Classroom" pop-up and overlay
+document.getElementById("createClassButton").addEventListener("click", function() {
+    document.getElementById("create-classroom").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+});
+
+// Show the "Join Classroom" pop-up and overlay
+document.getElementById("joinClassButton").addEventListener("click", function() {
+    document.getElementById("join-classroom").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+});
+
+function checkClassCode() {
+    // Get the value of the class code input
+    const classCode = document.getElementById("class-code").value.trim().toLowerCase();
+
+    // Check the entered class code and redirect based on the code
+    if (classCode === "/ping") {
+        alert("ping succesful✅");
+    } 
+    else if (classCode === "plexy") {
+        window.location.replace("plmath/index.html");
+    }
+    else if (classCode === "pizza") {
+        window.location.replace("pizzamath/index.html");
+    }
+    else {
+        alert("Something went wrong\nCouldn't join the class. Invalid Code. You can find codes here \n\nhttps://sites.google.com/view/goclassroomcodes/home")
+        window.location.href = "index.html";
+    }
+}
